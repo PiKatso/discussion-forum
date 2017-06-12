5c081b212918fcde4d11ed8f5b8358a7621d6ff9
Kat Gifford and Steven Galvin <kat@>
2017-06-12 11:56:32 -0700

    add ability to list categories. begin writing code to add new categories.

--- src/app/category-list/category-list.component.ts
+++ src/app/category-list/category-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Category } from '../category.model';
 
 @Component({
@@ -8,5 +8,12 @@ import { Category } from '../category.model';
 })
 export class CategoryListComponent {
  @Input() childCategoryList: Category[];
+ @Output() newCategorySender = new EventEmitter();
+
+ createNewCategory(name: string, description: string) {
+   var newCategory: Category = new Category(name, description);
+   this.newCategorySender.emit(newCategory);
+   console.log(newCategory);
+ }
 
 }
